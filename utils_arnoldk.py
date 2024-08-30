4553c745183b2887b3562a140be389fdfa061168
Arnold Atchoe <kwameape123@gmail.com>
2024-08-30 18:14:03 -0500

    Iteration 2-improve

--- utils_arnoldk.py
+++ utils_arnoldk.py
@@ -1,4 +1,4 @@
-''' ITERATION 1
+''' ITERATION 2
 
 Module: Alpha Analytics - Reusable Module for My Data Analytics Projects
 
@@ -7,21 +7,20 @@ When we work hard to write useful code, we want it to be reusable.
 A good byline could be used in every Python analytics project we do.
 
 Process:
+In this second iteration,add a function that returns the byline as a string.
 
-We don't write code from top to bottom; instead, we often write it from the outside in.
-Here's what a first draft of my utils_arnoldk.py might look like:
-
-1. I start with this docstring at the very beginning.
-   I use it to clarify the purpose of my Python file and organize my thoughts.
+1.I'll create a function named get_byline()
    
-2. I'll declare a global variable for my byline string and just set it to some simple text.
+2. I'll return my byline to whatever calls the get_byline() function .
 
-3. I'll declare a main() function for my module. When I run this script, I can use main() to test my byline.
+3. I'll update the main() function to use the new get_byline() function.
 
-4. I'll add the boilerplate conditional execution code so I only run the main() function when 
-   this script is executed directly (but not when I import it into another file).
+4. Same conditional boilerplate at the end.
 
-I'll test it in an online interpreter to ensure this version runs correctly before continuing.
+I'll test this version in an online interpreter to ensure this version runs correctly before continuing.
+It will demonstrate my ability to:
+-declare variables of different types.
+-Use Python to calculate basic descriptive statistics.
 '''
 
 #####################################
@@ -31,24 +30,25 @@ I'll test it in an online interpreter to ensure this version runs correctly befo
 byline: str = 'Alpha Analytics: Delivering Transforming Insights'
 
 #####################################
+# Define the get_byline() Function.
+#####################################
+
+def get_byline()->str:
+    '''Return a byline for my analytics projects.'''
+    return byline 
+
+#####################################
 # Define a main() function for this module.
 #####################################
 
-# Create a function named main.
-# A function is a block of code that performs a specific task.
-# This function will simply print the byline to the console.
-# Add a type hint to indicate that this function doesn't return anything when called 
-# (that is, it has a Python type of None).
-# It doesn't need any additional information passed in, 
-# so there's nothing needed inside the parentheses.
-# Everything afer the colon (:) must be indented (usually 4 spaces)
+# The main function now calls get_byline() to retrieve the byline.
 
 def main() -> None:
     '''Print the byline to the console when this function is called.'''
-    print(byline)
+    print(get_byline())
 
 #####################################
-# Conditional Execution - Only call main() when executing this module as a script.
+# Conditional Execution
 #####################################
 
 if __name__ == '__main__':
